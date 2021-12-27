46330df9da2a2b2868429fe692b57652deba0b7d
Hacker Hero <67130996+pr13260@users.noreply.github.com>
2021-12-28 00:46:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 w = open('config.json', 'w+')
 w.write('{')
 w.write('\n')
-w.write('    "authToken": "'+os.getenv('BOT_TOKEN')+'",')
+w.write('    "authToken": "5086523690:AAF5wfd-YNUGnFEliqIBDo3OamUW2M96JVQ",
 w.write('\n')
-w.write('    "owner": '+os.getenv('OWNER_ID'))
+w.write('    "owner": 1154075796)
 w.write('\n')
 w.write('}')
